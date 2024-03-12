dbd4f7e85fcc6325226bcbd0658bc4ab071a660d
Sartori Patrick <patrick.sartori@student.supsi.ch>
2024-03-12 15:21:39 +0100

    Added Read mode

--- peripheral_vision/lib/main.dart
+++ peripheral_vision/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/animation.dart';
+import 'package:flutter/widgets.dart';
 
 late List<CameraDescription> cameras;
 // function to trigger build when the app is run
@@ -69,8 +70,6 @@ class _SecondRouteState extends State<SecondRoute>
 
   @override
   void initState() {
-    
-
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     _setOrientation(ScreenOrientation.landscapeOnly);
     super.initState();
@@ -123,7 +122,7 @@ class _SecondRouteState extends State<SecondRoute>
           //print(_visible);
         },
         child: Scaffold(
-          extendBodyBehindAppBar:true,
+            extendBodyBehindAppBar: true,
             appBar: SlidingAppBar(
                 controller: _controller,
                 visible: _visible,
@@ -132,24 +131,25 @@ class _SecondRouteState extends State<SecondRoute>
                   backgroundColor: Colors.green,
                 )),
             body: Expanded(
-            child:Row(children: [
-              Flexible(
-                flex: 10,
-                child:Container(
-                height: double.infinity,
-                child: CameraPreview(_cameraController),
-                
-              )),
-              Spacer(
-                flex: 1,
-              ),
-              Flexible(
-                flex: 10,
-                child:Container(
-                height: double.infinity,
-                child: CameraPreview(_cameraController),
-              )),
-            ],
+                child: Row(
+              children: [
+                Flexible(
+                    flex: 10,
+                    child: AspectRatio(
+                      aspectRatio: _cameraController.value.aspectRatio,
+                      child: CameraPreview(_cameraController),
+                    )),
+                Spacer(
+                  flex: 1,
+                ),
+                Flexible(
+                  flex: 10,
+                  child: AspectRatio(
+                    aspectRatio: _cameraController.value.aspectRatio,
+                    child: CameraPreview(_cameraController),
+                  ),
+                ),
+              ],
             ))));
   }
 }
@@ -160,13 +160,47 @@ class ThirdRoute extends StatefulWidget {
   State<ThirdRoute> createState() => _ThirdRouteState();
 }
 
-class _ThirdRouteState extends State<ThirdRoute> {
+class _ThirdRouteState extends State<ThirdRoute>
+    with SingleTickerProviderStateMixin {
+  late CameraController _cameraController;
+  late CameraController _bottomCameraController;
+
+  bool _visible = true;
+  late final AnimationController _controller;
+  final size = Size(1920, 1080);
+
   @override
   void initState() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: [SystemUiOverlay.top]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     _setOrientation(ScreenOrientation.landscapeOnly);
     super.initState();
+    _cameraController = CameraController(cameras[0], ResolutionPreset.max);
+    _bottomCameraController =
+        CameraController(cameras[0], ResolutionPreset.max);
+    _cameraController.initialize().then((_) {
+      _bottomCameraController.value =
+          _cameraController.value.copyWith(previewSize: size);
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            print("access denied");
+            break;
+          default:
+            print(e.description);
+            break;
+        }
+      }
+    });
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
   }
 
   @override
@@ -180,12 +214,65 @@ class _ThirdRouteState extends State<ThirdRoute> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Tap Me Page"),
-        backgroundColor: Colors.green,
-      ), // AppBar
-    ); // Scaffold
+    return GestureDetector(
+        onTap: () {
+          print("TAP");
+          setState(() => _visible = !_visible);
+
+          if (_visible) {
+            print("Visible");
+          }
+          //print(_visible);
+        },
+        child: Scaffold(
+            extendBodyBehindAppBar: true,
+            appBar: SlidingAppBar(
+                controller: _controller,
+                visible: _visible,
+                child: AppBar(
+                  title: const Text("Camera Page"),
+                  backgroundColor: Colors.green,
+                )),
+            body: Expanded(
+                child: Row(
+              children: [
+                Flexible(
+                    flex: 10,
+                    child: Column(children: [
+                      Flexible(
+                          flex: 2,
+                          child: AspectRatio(
+                            aspectRatio: _cameraController.value.aspectRatio,
+                            child: CameraPreview(_cameraController),
+                          )),
+                      Flexible(
+                    flex: 1,
+                    child: AspectRatio(
+                      aspectRatio:  _cameraController.value.aspectRatio,
+                    child: CameraPreview(_cameraController),
+                    )),
+                    ])),
+                Spacer(
+                  flex: 1,
+                ),
+                Flexible(
+                    flex: 10,
+                    child: Column(children: [
+                      Flexible(
+                          flex: 2,
+                          child: AspectRatio(
+                            aspectRatio: _cameraController.value.aspectRatio,
+                            child: CameraPreview(_cameraController),
+                          )),
+Flexible(
+                    flex: 1,
+                    child: AspectRatio(
+                      aspectRatio:  _cameraController.value.aspectRatio,
+                    child: CameraPreview(_cameraController),
+                    )),
+                    ])),
+              ],
+            ))));
   }
 }
 
